98c407d2b7e619df8770f055caff03dbdf81cbac
Adriano dos Reis Carvalho <adriano.carvalho@ifsuldeminas.edu.br>
2023-03-02 16:53:27 -0300

    adicionando funções e transferência de textura

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from quilting import * 
+from quilting import *
+from texture_transfer import *
+
 
 texture_1 = plt.imread('C:\\Users\\adria\\Documents\\Mestrado\\image_quilting_adriano\\data\\texture1.jpg').astype(np.float32)
 
 block_size = 100
 overlap_size = int(block_size / 6)
 
+
 show_fig2c(synth_texture(texture_1, block_size, overlap_size))
+
+source_texture = plt.imread('data/texture6.jpg').astype(np.float32)
+target_image = plt.imread('data/bill.jpg').astype(np.float32)
+
+block_size = 30
+overlap_size = int(block_size / 6)
+
+show_text_trans(transfer_texture(source_texture, target_image, block_size))
 
--- quilting.py
+++ quilting.py
@@ -272,6 +272,177 @@ def synth_texture(src_texture, blk_size, overlap_size):
 
     return target
 
+def synth_texture_rand(texture, blk_size):
+    h, w, c = texture.shape
+    assert blk_size < min(h, w)
+
+    y_max, x_max = h - blk_size, w - blk_size
+
+    # desired size of new image is twice original one
+    dh = h * 2
+    dw = w * 2
+
+    nx_blocks = ny_blocks = max(dh, dw) // blk_size
+    w_new = h_new = nx_blocks * blk_size
+
+    n_blocks = nx_blocks * ny_blocks
+
+    texture_img = np.zeros((h_new, w_new, c), dtype=texture.dtype)
+
+    # Choose random blocks
+    xs = np.random.randint(0, x_max, size=n_blocks)
+    ys = np.random.randint(0, y_max, size=n_blocks)
+    ind = np.vstack((xs, ys)).T
+
+    blocks = np.array([texture[y:y + blk_size, x:x + blk_size] for x, y in ind])
+
+    b = 0
+    for y in range(ny_blocks):
+        for x in range(nx_blocks):
+            x1, y1 = x * blk_size, y * blk_size
+            x2, y2 = x1 + blk_size, y1 + blk_size
+            texture_img[y1:y2, x1:x2] = blocks[b]
+            b += 1
+
+    return texture_img
+
+
+def synth_texture_neighborhood(texture, blk_size, overlap_size):
+    h, w, c = texture.shape
+
+    assert blk_size < min(h, w)
+
+    # desired size of new image is twice original one
+    dh = h * 2
+    dw = w * 2
+
+    y_max, x_max = h - blk_size, w - blk_size
+    nx_blocks = ny_blocks = max(dh, dw) // blk_size
+    w_new = h_new = nx_blocks * blk_size - (nx_blocks - 1) * overlap_size
+
+    xs = np.arange(x_max)
+    ys = np.arange(y_max)
+    all_blocks = np.array([texture[y:y + blk_size, x:x + blk_size] for x in xs for y in ys])
+
+    target_height = h_new
+    target_width = w_new
+    target = np.zeros((target_height, target_width, c), dtype=texture.dtype)
+
+    step = blk_size - overlap_size
+
+    y_begin = 0
+    y_end = blk_size
+
+    for y in range(ny_blocks):
+
+        x_begin = 0
+        x_end = blk_size
+
+        for x in range(nx_blocks):
+            if x == 0 and y == 0:
+                # randomly select top left patch
+                r = np.random.randint(len(all_blocks))
+                random_patch = all_blocks[r]
+                target[y_begin:y_end, x_begin:x_end] = random_patch
+
+                x_begin = x_end
+                x_end += step
+
+                continue
+
+            xa, xb = x_begin - blk_size, x_begin
+            ya, yb = y_begin - blk_size, y_begin
+
+            if y == 0:
+                y1 = 0
+                y2 = blk_size
+
+                left_patch = target[y1:y2, xa:xb]
+                left_block = left_patch[:, -overlap_size:]
+                left_cost = l2_left_right(patch_left=left_patch, patch_right=all_blocks)
+                best_right_patch = select_min_patch(all_blocks, left_cost)
+
+                best_right_block = best_right_patch[:, :overlap_size]
+
+                # join left and right blocks
+                left_right_join = np.hstack(
+                    (left_block[:, :overlap_size // 2], best_right_block[:, overlap_size // 2:]))
+                full_join = np.hstack(
+                    (target[y1:y2, xa:xb - overlap_size], left_right_join, best_right_patch[:, overlap_size:]))
+
+                target[y1:y2, xa:x_end] = full_join
+            else:
+                if x == 0:
+                    x1 = 0
+                    x2 = blk_size
+                    top_patch = target[ya:yb, x1:x2]
+                    top_block = top_patch[-overlap_size:, :]
+                    top_cost = l2_top_bottom(patch_top=top_patch, patch_bottom=all_blocks)
+                    best_bottom_patch = select_min_patch(all_blocks, top_cost)
+                    best_bottom_block = best_bottom_patch[:overlap_size, :]
+
+                    # join top and bottom blocks
+                    top_bottom_join = np.vstack(
+                        (top_block[:overlap_size // 2, :], best_bottom_block[overlap_size // 2:, :]))
+                    full_join = np.vstack(
+                        (target[ya:yb - overlap_size, x1:x2], top_bottom_join, best_bottom_patch[overlap_size:, :]))
+
+                    target[ya:y_end, x1:x2] = full_join
+                else:
+                    # overlap is L-shaped
+                    y1, y2 = y_begin - overlap_size, y_end
+                    x1, x2 = x_begin - overlap_size, x_end
+
+                    left_patch = target[y1:y2, xa:xb]
+                    top_patch = target[ya:yb, x1:x2]
+
+                    left_block = left_patch[:, -overlap_size:]
+                    top_block = top_patch[-overlap_size:, :]
+
+                    left_cost = l2_left_right(patch_left=left_patch, patch_right=all_blocks)
+                    top_cost = l2_top_bottom(patch_top=top_patch, patch_bottom=all_blocks)
+
+                    best_right_patch = best_bottom_patch = select_min_patch(all_blocks, top_cost + left_cost)
+
+                    best_right_block = best_right_patch[:, :overlap_size]
+                    best_bottom_block = best_bottom_patch[:overlap_size, :]
+
+                    # join left and right blocks
+                    left_right_join = np.hstack(
+                        (left_block[:, :overlap_size // 2], best_right_block[:, overlap_size // 2:]))
+                    full_lr_join = np.hstack(
+                        (target[y1:y2, xa:xb - overlap_size], left_right_join, best_right_patch[:, overlap_size:]))
+
+                    # join top and bottom blocks
+                    top_bottom_join = np.vstack(
+                        (top_block[:overlap_size // 2, :], best_bottom_block[overlap_size // 2:, :]))
+                    full_tb_join = np.vstack(
+                        (target[ya:yb - overlap_size, x1:x2], top_bottom_join, best_bottom_patch[overlap_size:, :]))
+
+                    target[ya:y_end, x1:x2] = full_tb_join
+                    target[y1:y2, xa:x_end] = full_lr_join
+
+            x_begin = x_end
+            x_end += step
+
+        y_begin = y_end
+        y_end += step
+
+    return target
+
+def show_fig2a(texture_img):
+    plt.title('Figure 2a')
+    plt.imshow(normalize_img(texture_img))
+    plt.axis('off')
+    plt.show()
+
+
+def show_fig2b(texture_img):
+    plt.title('Figure 2b')
+    plt.imshow(normalize_img(texture_img))
+    plt.axis('off')
+    plt.show()
+
 def show_fig2c(texture_img):
     plt.title('Figure 2c')
     plt.imshow(normalize_img(texture_img))
